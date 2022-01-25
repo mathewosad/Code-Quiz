05f725bc9044772d3c753384743ee9cab6d5db43
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-01-24 23:25:17 -0800

    updated my entire work

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
-# Fruit-Quiz
+# Fruits Quiz
 
-Add screenshot
-add url for github
-add link to deployment
+AS A coding boot camp student i was given a task to make a unique online quiz that stores high scores and i could compare my progress/result to my peers.
+
+# Critarias to meet 
+
+The web page is ease to use:
+
+When user clicks the start button,
+user is taking the quiz and the timer starts with a question presented.
+when a question is answered user is presented with another question.
+when answer is incorrect time is subtracted from the clock
+when timer reaches zero hopefully the game is over 
+user can register initials for hightscores and high score history
+
+# Descriptions 
+* Application deployed at live URL.(on git hub)
+
+* Application loads with no errors.
+
+* Application GitHub URL submitted and here is a link 
+
+* GitHub repository contains application code.
+
+
+Application user experience is intuitive and easy to navigate.
+
+* Application user interface style is clean and polished.
+
+Repository has a unique name. Fruits Quiz.
+
+* Repository contains multiple descriptive commit messages.
+
+* Readme file contains description above , screenshot (below), and link to deployed application.
+
+Thank you.
+
+
+© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const rules = document.querySelector(".rules");
 const exit_btn = rules.querySelector(".buttons .quit");
 const continue_btn = rules.querySelector(".buttons .continue");
 const quiz = document.querySelector(".quiz");
+const timecount = quiz.querySelector("#timer_sec");
+const option_list = document.querySelector("#option_list");
 
 // if start quiz button is clicked 
 
@@ -21,6 +23,8 @@ continue_btn.onclick = () => {
     rules.classList.remove("activeInfo"); // removes rules 
     quiz.classList.add("activeQuiz"); // this shows quiz page
     showQuestions(0); // Added from line 101
+    queCounter(1);
+    startTime(60);
 }
 
 // Questions for my Quiz
@@ -45,7 +49,7 @@ let questions = [
         question: "What color is broccoli?",
         answer: "Green",
         options: [
-            "Yellow ",
+            "Yellow",
             "Green",
             "Blue",
             "Red"
@@ -54,7 +58,7 @@ let questions = [
     {
         num: 2,
         question: "What color is banana?",
-        answer: "Yello",
+        answer: "Yellow",
         options: [
             "Yellow ",
             "Green",
@@ -72,12 +76,13 @@ let questions = [
             "Blue",
             "Red"
         ]
-    }, {
+    }, 
+    {
         num: 4,
         question: "What color is Strawberry?",
         answer: "Red",
         options: [
-            "Red ",
+            "Red",
             "Green",
             "Blue",
             "Yellow"
@@ -88,7 +93,7 @@ let questions = [
         question: "What color is pear?",
         answer: "Green",
         options: [
-            "Blue ",
+            "Blue",
             "Yellow",
             "Green",
             "Red"
@@ -97,31 +102,103 @@ let questions = [
 ];
 
 var que_count = 0;
+var que_num = 1
+var counter;
+
+const next_btn = quiz.querySelector("#next_btn");
+const result = document.querySelector(".result");
+const submit = result.querySelector(".submit");
+
+const result2 = document.querySelector(".result2");
+const replay = result.querySelector(".replay");
+const quit = quiz.querySelector(".quit")
 
 // Next button when clicked 
 
-const next_btn = quiz.querySelector("#next_btn");
-next_btn.onclick = ()=>{
-    if(que_count < questions.length - 1){
+
+next_btn.onclick = () => {
+    if (que_count < questions.length - 1) {
         que_count++;
+        que_num++;
         showQuestions(que_count);
-    }else{
-        console.log("Questions completed")
+        queCounter(que_num);
+    } else {
+        console.log("Questions completed");
+        showresult();
     }
 }
 
 // Get each questions, option and answer array
-function showQuestions(index){
+function showQuestions(index) {
     const que_text = document.querySelector("#que_text");
-    const option_list = document.querySelector("#option_list");
-    var que_tag = '<span>'+ questions[index].question +'</span>'; 
+    var que_tag = '<span>' + questions[index].question + '</span>';
     // try childnodes? append child? i had to input the whole Div tag
 
-    var option_tag = '<div id="option"'+ questions[index].options[0] + '<span></span></div>'
-                     + '<div id="option"'+ questions[index].options[1] + '<span></span></div>'
-                     + '<div id="option"'+ questions[index].options[2] + '<span></span></div>'
-                     + '<div id="option"'+ questions[index].options[3] + '<span></span></div>';
-    que_text.innerHTML = que_tag;
+    var option_tag = '<div id="option">' + questions[index].options[0] + '<span></span></div>'
+        + '<div id="option">' + questions[index].options[1] + '<span></span></div>'
+        + '<div id="option">' + questions[index].options[2] + '<span></span></div>'
+        + '<div id="option">' + questions[index].options[3] + '<span></span></div>';
+
     option_list.innerHTML = option_tag;
-    console.log(showQuestions(index));
+    que_text.innerHTML = que_tag;
+
+    const option = option_list.querySelectorAll("#option")
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+// Answers 
+var wrongIcon = '<div class="icon wrong"><i class="fas fa-times"></i></div>';
+var rightIcon = '<div class="icon right"><i class="far fa-check-circle"></i></div>';
+function optionSelected(answer) {
+    var userAns = answer.textContent;
+    var correctAns = questions[que_count].answer;
+    var allOptions = option_list.children.length;
+    if (userAns == correctAns) {
+        answer.classList.add("correct");
+        console.log("Answer is correct");
+        answer.insertAdjacentHTML("beforeend",rightIcon);
+    } else{
+        answer.classList.add("incorrect");
+        answer.insertAdjacentHTML("beforeend",wrongIcon);
+        console.log("Answer is wrong");
+        // if answers is incorrect then automatically select 
+    }
+    for (let i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disable");
+
+    }
+}
+
+function showresult(){
+    rules.classList.remove("activeInfo");
+    quiz.classList.remove("activeQuiz");
+    result.classList.add("activeResult"); //show result page
+}
+
+submit.onclick = () =>{
+    rules.classList.remove("activeInfo");
+    quiz.classList.remove("activeQuiz");
+    result.classList.remove("activeResult");
+    result2.classList.add("activeResult2");//show the highscores
+} 
+
+
+function startTime(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timecount.textContent = time;
+        time--;
+        if (time < 0){
+            clearInterval(counter);
+            timecount.textContent = "00";
+        }
+    }
+}
+
+function queCounter(index) {
+    const bottom_que_counter = quiz.querySelector("#total_q");
+    var totalQuesCountTag = '<span><p>' + index + '</p>of<p>' + questions.length + '</p>Questions</span>';
+    bottom_que_counter.innerHTML = totalQuesCountTag;
 }
