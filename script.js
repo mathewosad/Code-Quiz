6772d013935f17e76b6b453b7c16bdb3b3a7e91f
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-01-25 23:16:17 -0800

    last update

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// all required Dom elements
-const start_btn = document.querySelector(".start_btn button");
-const rules = document.querySelector(".rules");
-const exit_btn = rules.querySelector(".buttons .quit");
-const continue_btn = rules.querySelector(".buttons .continue");
-const quiz = document.querySelector(".quiz");
-const timecount = quiz.querySelector("#timer_sec");
-const option_list = document.querySelector("#option_list");
-
-// if start quiz button is clicked 
-
-start_btn.onclick = () => {
-    rules.classList.add("activeInfo");
-}//shows the rules 
-
-// if exit button is clicked 
-
-exit_btn.onclick = () => {
-    rules.classList.remove("activeInfo");
-} //when clicked rules get hidden 
-
-continue_btn.onclick = () => {
-    rules.classList.remove("activeInfo"); // removes rules 
-    quiz.classList.add("activeQuiz"); // this shows quiz page
-    showQuestions(0); // Added from line 101
-    queCounter(1);
-    startTime(60);
-}
-
 // Questions for my Quiz
 // What color is broccoli?
 // What color is spinach?
@@ -38,11 +9,42 @@ continue_btn.onclick = () => {
 // Green 
 // Blue 
 // Red
-
 // creating an array for 
 // numbers
 // questions
-// Options and answers
+// Options and answers// all required Dom elements
+const start_btn = document.querySelector(".start_btn button");
+const rules = document.querySelector(".rules");
+const exit_btn = rules.querySelector(".buttons .quit");
+const continue_btn = rules.querySelector(".buttons .continue");
+const quiz = document.querySelector(".quiz");
+const timecount = quiz.querySelector("#timer_sec");
+const option_list = document.querySelector("#option_list");
+const next_btn = quiz.querySelector("#next_btn");
+const result = document.querySelector(".result");
+const submit = result.querySelector(".submit");
+const result2 = document.querySelector(".result2");
+const initials = document.querySelector(".initials");
+const replay = result.querySelector(".replay");
+const quit_quiz = document.querySelector(".quit_quiz");
+
+// if start quiz button is clicked 
+start_btn.onclick = () => {
+    rules.classList.add("activeInfo");//shows the rules 
+}
+// if exit button is clicked 
+exit_btn.onclick = () => {
+    rules.classList.remove("activeInfo");
+} 
+//when clicked rules get hidden 
+continue_btn.onclick = () => {
+    rules.classList.remove("activeInfo"); // removes rules 
+    quiz.classList.add("activeQuiz"); // this shows quiz page
+    showQuestions(0); // Added from line 101
+    queCounter(1);
+    startTime();
+}
+
 let questions = [
     {
         num: 1,
@@ -60,7 +62,7 @@ let questions = [
         question: "What color is banana?",
         answer: "Yellow",
         options: [
-            "Yellow ",
+            "Yellow",
             "Green",
             "Blue",
             "Red"
@@ -71,12 +73,12 @@ let questions = [
         question: "What color is spinach?",
         answer: "Green",
         options: [
-            "Yellow ",
+            "Yellow",
             "Green",
             "Blue",
             "Red"
         ]
-    }, 
+    },
     {
         num: 4,
         question: "What color is Strawberry?",
@@ -100,22 +102,12 @@ let questions = [
         ]
     },
 ];
-
 var que_count = 0;
 var que_num = 1
 var counter;
-
-const next_btn = quiz.querySelector("#next_btn");
-const result = document.querySelector(".result");
-const submit = result.querySelector(".submit");
-
-const result2 = document.querySelector(".result2");
-const replay = result.querySelector(".replay");
-const quit = quiz.querySelector(".quit")
+var userScore = 0;
 
 // Next button when clicked 
-
-
 next_btn.onclick = () => {
     if (que_count < questions.length - 1) {
         que_count++;
@@ -149,21 +141,24 @@ function showQuestions(index) {
 }
 
 // Answers 
-var wrongIcon = '<div class="icon wrong"><i class="fas fa-times"></i></div>';
-var rightIcon = '<div class="icon right"><i class="far fa-check-circle"></i></div>';
+var wrongIcon = '<div class="icon wrong"><i class="fas fa-times"></i></div>';//icon 
+var rightIcon = '<div class="icon right"><i class="far fa-check-circle"></i></div>';//icon 
 function optionSelected(answer) {
     var userAns = answer.textContent;
     var correctAns = questions[que_count].answer;
     var allOptions = option_list.children.length;
     if (userAns == correctAns) {
+        userScore += 1;
+        console.log(userScore);
         answer.classList.add("correct");
         console.log("Answer is correct");
-        answer.insertAdjacentHTML("beforeend",rightIcon);
-    } else{
+        answer.insertAdjacentHTML("beforeend", rightIcon);//icon 
+    } else {
         answer.classList.add("incorrect");
-        answer.insertAdjacentHTML("beforeend",wrongIcon);
+        answer.insertAdjacentHTML("beforeend", wrongIcon); //icon 
         console.log("Answer is wrong");
-        // if answers is incorrect then automatically select 
+        time -= 10;
+        // if answers is incorrect then automatically deduct 10 seconds 
     }
     for (let i = 0; i < allOptions; i++) {
         option_list.children[i].classList.add("disable");
@@ -171,34 +166,67 @@ function optionSelected(answer) {
     }
 }
 
-function showresult(){
-    rules.classList.remove("activeInfo");
-    quiz.classList.remove("activeQuiz");
-    result.classList.add("activeResult"); //show result page
-}
-
-submit.onclick = () =>{
-    rules.classList.remove("activeInfo");
-    quiz.classList.remove("activeQuiz");
-    result.classList.remove("activeResult");
-    result2.classList.add("activeResult2");//show the highscores
-} 
-
-
-function startTime(time){
-    counter = setInterval(timer, 1000);
-    function timer(){
-        timecount.textContent = time;
-        time--;
-        if (time < 0){
-            clearInterval(counter);
-            timecount.textContent = "00";
-        }
-    }
-}
-
+//  number of questions 
 function queCounter(index) {
     const bottom_que_counter = quiz.querySelector("#total_q");
     var totalQuesCountTag = '<span><p>' + index + '</p>of<p>' + questions.length + '</p>Questions</span>';
     bottom_que_counter.innerHTML = totalQuesCountTag;
+}
+// Timer 
+var time = 60;
+function startTime() {
+    counter = setInterval(timer, 1000);
+    function timer() {
+        timecount.textContent = time;
+        time--;
+        if (time <= 0) {
+            clearInterval(counter);
+            timecount.textContent = "0";
+            showresult();
+        }
+
+    }
+}
+
+function showresult() {
+    rules.classList.remove("activeInfo");
+    quiz.classList.remove("activeQuiz");
+    result.classList.add("activeResult"); //show result page
+    const scoreText = result.querySelector("#score");
+    if (userScore > 3) {
+        let scoreTag = '<span>Congrats! you got <p>' + userScore + '</p>Out of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if (userScore > 1) {
+        let scoreTag = '<span>Congrats! you got <p>' + userScore + '</p>Out of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else {
+        let scoreTag = '<span>Congrats! you got <p>' + userScore + '</p>Out of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+
+}
+submit.onclick = () => {
+    rules.classList.remove("activeInfo");
+    quiz.classList.remove("activeQuiz");
+    result.classList.remove("activeResult");
+    result2.classList.add("activeResult2");
+    localStorage.setItem("initials", JSON.stringify(initials.value));
+         function renderLastinitials() {
+            // Use JSON.parse() to convert text to JavaScript object
+            var Lastinitials = JSON.parse(localStorage.getItem("initials"));
+            if (Lastinitials !== null){
+            localStorage.getItem("initials", JSON.stringify(initials));
+            document.getElementById("saved_comments").innerHTML = Lastinitials.initials;
+            }
+        }
+
+    }   
+//show the highscores
+
+// when clicked it reloads the page 
+
+quit_quiz.onclick = () => {
+    window.location.reload();
 }
